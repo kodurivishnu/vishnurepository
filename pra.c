ac12c28504d1672b8de70dfe804630614df56a4b
aisha1 <arpitamohapatra0013@gmail.com>
2017-12-09 12:00:15 +0530

    iedit pra.c

--- pra.c
+++ pra.c
@@ -4,5 +4,8 @@ void main()
 	printf("haii");
 	printf("Prathusha this is message for you");
 	printf("Hello");
+	printf("PRATYUSHA LOVES ARPITA");
+	printf("ARPITA LOVES KAVYA ");
+	printf("KAVYA LOVES PRATYUSHA");
 }
 
